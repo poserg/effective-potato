876acc805c99f507832dd6ccfeaacd0247d591e1
Sergei Popov <spopov@cinimex.ru>
2022-08-13 07:07:19 +0300

    Replace list of categories to set

--- potato.py
+++ potato.py
@@ -1,8 +1,7 @@
 class Category:
-	def __init__(self, title, sum, account_guid):
+	def __init__(self, title, sum):
 		self.title = title
 		self.sum = sum
-		self.account_guid = account_guid
 
 def get_active_categories(all_catetories, sum):
 	return [x for x in all_catetories if x.sum <= sum]
--- test_potato.py
+++ test_potato.py
@@ -7,17 +7,17 @@ class TestGetActiveCategory(unittest.TestCase):
 
 	def test_get_zero_active_categories(self):
 		result = get_active_categories([
-			Category('category 1', 1000, ''),
-			Category('category 2', 1500, '')
+			Category('category 1', 1000),
+			Category('category 2', 1500)
 			],
 			500)
 		self.assertEqual(result, [])
 
 	def test_get_some_active_categories(self):
-		expected_category = Category('category 1', 1000, '')
+		expected_category = Category('category 1', 1000)
 		result = get_active_categories([
 			expected_category,
-			Category('category 2', 1500, '')
+			Category('category 2', 1500)
 			],
 			1200)
 		self.assertEqual(result, [expected_category])
